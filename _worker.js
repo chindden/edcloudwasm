5a140a1ec9898673f9d6bb68757ec9d3595c21f6
chindden <34678156+chindden@users.noreply.github.com>
2026-03-06 18:25:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -34,7 +34,7 @@ const proxyIpAddrs = {EU: 'ProxyIP.DE.CMLiussss.net', AS: 'ProxyIP.SG.CMLiussss.
 const finallyProxyHost = 'ProxyIP.CMLiussss.net';//兜底proxyip
 // 订阅和面板使用的优选ip地址
 const ipListAll = [
-    'fbi.gov#US','shopify.com','[2606:4700::]','cn.tencentapp.cn','saas.sin.fan','download.yunzhongzhuan.com','172.64.151.241', '172.64.153.2', '104.18.39.123', '104.18.42.218', '172.64.154.125', '104.18.36.15', '172.64.145.202', '172.64.149.99'
+    'fbi.gov','shopify.com','[2606:4700::]','cn.tencentapp.cn','saas.sin.fan','download.yunzhongzhuan.com','172.64.151.241', '172.64.153.2', '104.18.39.123', '104.18.42.218', '172.64.154.125', '104.18.36.15', '172.64.145.202', '172.64.149.99'
 ];
 const coloRegions = {
     JP: new Set(['FUK', 'ICN', 'KIX', 'NRT', 'OKA']),
