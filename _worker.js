dc8de4ca50a384ae44f2930f21792bc48e5a3160
chindden <34678156+chindden@users.noreply.github.com>
2026-05-13 00:08:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,4 +1,4 @@
-// 代码基本都抄的CM和AK大佬和天书大佬的项目，在此感谢各位大佬的无私奉献。
+// ❤️订阅地址：https://wassm.pages.dev/ef9d104e-ca0e-4202-ba4b-a0afb969c747
 import {connect} from 'cloudflare:sockets';
 const defaultUuid = ''; // 可在环境变量配置，变量名称为UUID，两个地方都不写为不验证uuid
 const defaultPassword = ''; // 可在环境变量配置，变量名称为PASSWORD，两个地方都不写为不验证密码
